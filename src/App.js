9616e864bc9ae4c44bacd94218ba42aee5482da9
santhu <santhuchary143@gmail.com>
2020-11-19 18:30:32 +0530

    Feature1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from "./components/Login"
+
+// import Register from "./components/Register"
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Login/> */}
+      <Login/>
+    
+    </div>
+  );
+}
+
+export default App;
